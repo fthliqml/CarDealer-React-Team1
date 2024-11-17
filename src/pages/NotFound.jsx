6f5225fdcb9bae9d4f3e2676d8719ac9001d09d6
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2024-11-17 15:52:06 +0800

    feat(register): Finish creating register page with all of validation

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,4 @@
 import Container from "@/components/ui/container";
-import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
@@ -8,13 +7,7 @@ function NotFound() {
         className={"flex flex-col justify-center items-center gap-y-5"}
       >
         <h1 className="text-3xl font-bold">404 Page Not Found</h1>
-        <h2 className="text-xl">
-          Go to{" "}
-          <Link to={"/home"} className="underline text-blue-800">
-            Home
-          </Link>{" "}
-          page
-        </h2>
+        <h2 className="text-xl">Please back to previous page</h2>
       </Container>
     </>
   );
